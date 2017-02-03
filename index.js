02f9384e16954f038938f668958494c1365c4866
sennrdesign <sennadeturner@gmail.com>
2017-02-03 22:45:23 +0100

    index.js update

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ var Output = document.getElementById("Output");
 $(document).ready(function(){
   $("#sendData").click(function(){
     alert("button clicked");
+    $.ajax({
+      url:'Trans.json',
+      dataType:'json',
+      cache:false,
+      success:function(data){
+        alert(data);
+      }
+    })
   });
 });
