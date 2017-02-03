46a06f3b45283c736113fffddfea9ea474f3881f
sennrdesign <sennadeturner@gmail.com>
2017-02-03 22:08:50 +0100

    new

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var Input = document.getElementById("Input");
 var Output = document.getElementById("Output");
 $('#sendData').click(function(){
-  $.getJSON("index.json", function(obj){
-    $.each(obj, function(key, value){
-      $("#Output").html()
-    });
+  $.ajax({
+    url:"Trans.json",
+    dataType:'json',
+    type:'get',
+    cache:false,
+    success:function(data){
+      var $object = $(data);
+    }
   });
 });
