5f0f170e06b7654bdd41d61b7cc0c99e8a2ff162
sennrdesign <sennadeturner@gmail.com>
2017-02-03 22:43:21 +0100

    new

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var Input = document.getElementById("Input");
 var Output = document.getElementById("Output");
 $(document).ready(function(){
-  alert("document ready");
   $("#sendData").click(function(){
     alert("button clicked");
   });
