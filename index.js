2f15e222907c0f870191cd2b8c526ea0b71e77f0
sennrdesign <sennadeturner@gmail.com>
2017-02-03 21:56:21 +0100

    new

---
+++ index.js
@@ -0,0 +1,7 @@
+var Input = document.getElementById("Input");
+var Output = document.getElementById("Output");
+$('#sendData').click(function(){
+  $.getJSON("index.json", function(obj){
+
+  });
+});
