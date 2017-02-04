4456a2ad922b77e7243e5ab7ec1fc5034994940c
sennrdesign <sennadeturner@gmail.com>
2017-02-04 08:42:51 +0100

    new

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
     $.ajax({
       url:'Trans.json',
       dataType:'json',
-      cache:false,
+      cache:true,
       success:function(data){
         var Input = $('#Input').val();
         var DataOutput = data[Input.toLowerCase()];
         if(typeof(DataOutput) == "object"){
           $('#Output').html(DataOutput[0]);
+          $('#Output').clicked(function(){
+            alert("output clicked!");
+          });
         }else{
           $('#Output').html(DataOutput);
         }
